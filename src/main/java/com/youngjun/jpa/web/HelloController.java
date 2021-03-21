63d738b035eb5c671749c8f6ad4fb25140795284
YoungJun Park <youngjun108059@gmail.com>
2021-03-21 23:58:20 +0900

    [Controller] API 파라미터로 DTO 생성 샘플

--- src/main/java/com/youngjun/jpa/web/HelloController.java
+++ src/main/java/com/youngjun/jpa/web/HelloController.java
@@ -13,5 +13,16 @@ public class HelloController {
         return "hello";
     }
 
-
+    /**
+     * 외부 API 에서 넘긴 파라미터로 Hello Dto 생성
+     *
+     * @param name   이름
+     * @param amount 잔액
+     * @return HelloResponseDto 객체
+     */
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 }
