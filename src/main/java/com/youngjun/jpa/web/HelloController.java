2b84801942e5ce3887e084df06f4585505bedf5c
YoungJun Park <youngjun108059@gmail.com>
2021-03-21 23:54:33 +0900

    [Controller] 샘플 컨트롤러 작성

---
+++ src/main/java/com/youngjun/jpa/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.youngjun.jpa.web;
+
+import com.youngjun.jpa.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+
+}
