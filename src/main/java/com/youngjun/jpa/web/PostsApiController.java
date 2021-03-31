cdacb77a72d8c1ae8175f4bd1b78af2b9b9f6769
YoungJun Park <youngjun108059@gmail.com>
2021-03-31 22:55:10 +0900

    [Controller] Posts API 컨트롤러 (게시글 등록)

---
+++ src/main/java/com/youngjun/jpa/web/PostsApiController.java
@@ -0,0 +1,19 @@
+package com.youngjun.jpa.web;
+
+import com.youngjun.jpa.service.posts.PostsService;
+import com.youngjun.jpa.web.dto.PostsSaveRequestsDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestsDto requestsDto) {
+        return postsService.save(requestsDto);
+    }
+
+
+}
