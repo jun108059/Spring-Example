b5f078e41ed2566ce3cde07585a03a8f51413d84
YoungJun Park <youngjun108059@gmail.com>
2021-04-03 00:02:27 +0900

    [Controller] 게시글 수정, 조회 API

--- src/main/java/com/youngjun/jpa/web/PostsApiController.java
+++ src/main/java/com/youngjun/jpa/web/PostsApiController.java
@@ -1,7 +1,9 @@
 package com.youngjun.jpa.web;
 
 import com.youngjun.jpa.service.posts.PostsService;
+import com.youngjun.jpa.web.dto.PostsResponseDto;
 import com.youngjun.jpa.web.dto.PostsSaveRequestsDto;
+import com.youngjun.jpa.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,5 +17,14 @@ public class PostsApiController {
         return postsService.save(requestsDto);
     }
 
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 
 }
