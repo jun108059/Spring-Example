85e490d95876f353658be4a0289468a684eb1237
YoungJun Park <youngjun108059@gmail.com>
2021-04-05 23:16:32 +0900

    [Service] 게시글 수정, 조회 로직 추가

--- src/main/java/com/youngjun/jpa/service/posts/PostsService.java
+++ src/main/java/com/youngjun/jpa/service/posts/PostsService.java
@@ -2,7 +2,9 @@ package com.youngjun.jpa.service.posts;
 
 import com.youngjun.jpa.domain.posts.Posts;
 import com.youngjun.jpa.domain.posts.PostsRepository;
+import com.youngjun.jpa.web.dto.PostsResponseDto;
 import com.youngjun.jpa.web.dto.PostsSaveRequestsDto;
+import com.youngjun.jpa.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestsDto requestsDto) {
         return postsRepository.save(requestsDto.toEntity()).getId();
+    }
+
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
